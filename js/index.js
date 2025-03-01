52a716a0a44083393fa9892e7d8805376255ba71
fairlite2u <pyl95001@byui.edu>
2025-03-01 11:32:15 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const greeting = document.getElementById("greeting");
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "50px";
+const username = window.prompt("What is your name?", "Jane");
+greeting.textContent = "Welcome to my website, " + username + "!";
+
+const myName = "Jennifer";
+console.log(myName);
